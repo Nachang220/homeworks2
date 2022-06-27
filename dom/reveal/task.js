a3f607290ffae74c10734306d034df16f764abae
Nachang220 <filil2003@yandex.ru>
2022-06-27 21:23:48 +0300

    Commit

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,13 +3,16 @@ const reveals = document.querySelectorAll(".reveal");
 const isInViewport = element => {
   const viewportHeight = window.innerHeight;
   const elementTop = element.getBoundingClientRect().top;
-  return elementTop < viewportHeight;
+  const elementBottom = element.getBoundingClientRect().bottom;
+  return elementTop < viewportHeight && elementBottom > 0;
 };
 
 document.addEventListener("scroll", () => {
   reveals.forEach(item => {
     if (isInViewport(item)) {
       item.classList.add("reveal_active");
+    } else {
+      item.classList.remove("reveal_active");
     }
   });
 });
